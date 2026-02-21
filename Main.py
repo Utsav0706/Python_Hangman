17c7eac0d0dfd03a719224bf432b530106e0d15e
Utsav0706 <utsavjivani.07@gmail.com>
2026-02-21 14:23:34 +0000

    Get a random word to guess

--- Main.py
+++ Main.py
@@ -1,6 +1,18 @@
 import random
 import art, words
 
+lives = 6
 
+print(art.logo)
 
+chosen_word = random.choice(words.word_list)
+print(chosen_word)
+
+placeHolder = ""
+word_length = len(chosen_word)
+
+for position in range(word_length):
+    placeHolder += "_"
+
+print("Word to guess: " + placeHolder)
 
